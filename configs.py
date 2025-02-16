aae8c9765318cb85744f0bb1867cc60e53d7d344
AlexChapin <122164689+AlexChapin@users.noreply.github.com>
2025-02-15 22:08:48 -0500

    Move User Inputs to Configs

---
+++ configs.py
@@ -0,0 +1,98 @@
+# User Input
+# Dertermines Whether to Start the Program to Accept Manual Inputs or Run a Preset Program
+# True Means Enable User Inputs
+# False Means Run Preprogramed Routine
+# This Value Will Be Overwritten by Any Startup Flags
+runmanual = True
+
+# Default Frequency On Startup
+startupfreq1 = 0  # Hz
+startupamp1 = 0  # Percent
+
+startupfreq2 = 0  # Hz
+startupamp2 = 0  # Percent
+
+# Frequency 1 Settings
+setting1freq = 15  # Hz
+setting1amp = 75  # Percent
+
+setting2freq = 16.5  # Hz
+setting2amp = 45  # Percent
+
+setting3freq = 17.5  # Hz
+setting3amp = 50  # Percent
+
+setting4freq = 19  # Hz
+setting4amp = 70  # Percent
+
+setting5freq = 45  # Hz
+setting5amp = 100  # Percent
+
+# Frequency 2 Settings
+setting1freq2 = 5  # Hz
+setting1amp2 = 100  # Percent
+
+setting2freq2 = 15  # Hz
+setting2amp2 = 75  # Percent
+
+setting3freq2 = 35  # Hz
+setting3amp2 = 75  # Percent
+
+setting4freq2 = 65  # Hz
+setting4amp2 = 50  # Percent
+
+setting5freq2 = 100  # Hz
+setting5amp2 = 20  # Percent
+
+# Slider Settings
+freq1sliderlow = 1  # Hz
+freq1sliderhigh = 100  # Hz
+
+freq2sliderlow = 1  # Hz
+freq2sliderhigh = 100  # Hz
+
+# Smallest Unit That The Slider Can Change By
+scaleresolution = 0.1  # Hz
+
+freq1buttonpreset = 100  # Percent
+freq2buttonpreset = 100  # Percent
+
+# Sweep Rates
+# Fast Sweep Rates are Default
+fastsweeprate1 = 1000000000  # Pitch / Second
+fastsweeprate2 = 1000000000  # Pitch / Second
+
+slowsweeprate1 = 2  # Pitch / Second
+slowsweeprate2 = 2  # Pitch / Second
+
+# Auto Settings
+autofreqsweeprate = 10000  # Pitch / Second
+autoampsweeprate = 10000  # Percent / Second
+
+dospecifictimer = False
+
+# ADJUST PLEASE
+
+autofreq1 = 22  # Hz
+autoamp1 = 7  # Percent
+autotime1 = 20  # Seconds
+
+autofreq2 = 17.5  # Hz
+autoamp2 = 3 # Percent
+autotime2 = 15  # Seconds
+
+autofreq3 = 22  # Hz
+autoamp3 = 7 # Percent
+autotime3 = 20  # Seconds
+
+autofreq4 = 17.5  # Hz
+autoamp4 = 3  # Percent
+autotime4 = 20  # Seconds
+
+autofreq5 = 16  # Hz
+autoamp5 = 10 # Percent
+autotime5 = 5  # Seconds
+
+autofreq6 = 40  # Hz
+autoamp6 = 50  # Percent
+autotime6 = 20  # Seconds
